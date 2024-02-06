b9dd447621f427713d3f9036f4dfd1fd06f51e28
Ikhbar Nurinsani <ikhbarnuri@gmail.com>
2024-02-07 00:10:37 +0700

    client: menambahkan router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,25 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import ErrorPage from "./components/ErrorPage.jsx";
+import Home from "./components/Home.jsx";
+import Movies from "./components/Movies.jsx";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <App/>
+        element: <App/>,
+        errorElement: <ErrorPage/>,
+        children: [
+            {
+                index: true,
+                element: <Home/>
+            },
+            {
+                path: '/movies',
+                element: <Movies/>
+            }
+        ]
     }
 ])
 
