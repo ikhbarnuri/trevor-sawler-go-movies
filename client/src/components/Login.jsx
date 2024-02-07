ba8d2da1163140425cddbfd9f4faf4f08637cda9
Ikhbar Nurinsani <ikhbarnuri@gmail.com>
2024-02-07 22:10:49 +0700

    client: menambahkan akses untu men-set jwt pada form login

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,53 +1,55 @@
 import {useState} from "react";
 import Input from "./form/Input.jsx";
+import {useOutletContext} from "react-router-dom";
 
 function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
+    const {setJwtToken} = useOutletContext();
+
     const handleSubmit = (event) => {
         event.preventDefault();
+        console.log("email/pass", email, password)
 
-        if (email === "admin@example.com"){
-
+        if (email === "admin@example.com") {
+            setJwtToken("abc")
         }
     }
 
-    return (
-        <div className="col-md-6 offset-md-3">
-            <h2>Login</h2>
+    return (<div className="col-md-6 offset-md-3">
+        <h2>Login</h2>
+        <hr/>
+
+        <form onSubmit={handleSubmit}>
+            <Input
+                title="Email Address"
+                type="email"
+                className="form-control"
+                name="email"
+                autoComplete="email-new"
+                onChange={(event) => setEmail(event.target.value)}
+            />
+
+            <Input
+                title="Password"
+                type="password"
+                className="form-control"
+                name="password"
+                autoComplete="password-new"
+                onChange={(event) => setPassword(event.target.value)}
+            />
+
             <hr/>
 
-            <form onSubmit={handleSubmit}>
-                <Input
-                    title="Email Address"
-                    type="email"
-                    className="form-control"
-                    name="email"
-                    autoComplete="email-new"
-                    onChange={(event) => setEmail(event.target.value)}
-                />
+            <Input
+                type="submit"
+                className="btn btn-primary"
+                value="Login"
+            />
 
-                <Input
-                    title="Password"
-                    type="password"
-                    className="form-control"
-                    name="password"
-                    autoComplete="password-new"
-                    onChange={(event) => setPassword(event.target.value)}
-                />
-
-                <hr/>
-
-                <Input
-                    type="submit"
-                    className="btn btn-primary"
-                    value="Login"
-                />
-
-            </form>
-        </div>
-    );
+        </form>
+    </div>);
 }
 
 export default Login;
