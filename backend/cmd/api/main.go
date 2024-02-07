baa9b7c0d186db2dcccdede3e36ad0495273fe5c
Ikhbar Nurinsani <ikhbarnuri@gmail.com>
2024-02-08 06:52:27 +0700

    backend: menambahkan chi package router

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -19,9 +19,7 @@ func main() {
 
 	log.Println("Starting application on port", port)
 
-	http.HandleFunc("/", Hello)
-
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
 	if err != nil {
 		log.Fatal(err)
 	}
--- backend/cmd/api/routes.go
+++ backend/cmd/api/routes.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"net/http"
+)
+
+func (app *application) routes() http.Handler {
+	mux := chi.NewMux()
+
+	mux.Use(middleware.Recoverer)
+
+	return mux
+}
