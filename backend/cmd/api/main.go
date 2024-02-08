e5eedf000150ec7025f23d0a1ad702a7794acb4b
Ikhbar Nurinsani <ikhbarnuri@gmail.com>
2024-02-08 15:09:48 +0700

    backend: menambhakan koneksi ke database

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,6 +12,8 @@ const port = 8080
 
 type application struct {
 	Domain string
+	DSN    string
+	DB     *sql.DB
 }
 
 func main() {
@@ -17,9 +21,18 @@ func main() {
 
 	app.Domain = "example.com"
 
+	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=movies sslmode=disable timezone=utc connect_timeout=5", "Postgres connection string")
+	flag.Parse()
+
+	conn, err := app.connectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	app.DB = conn
+
 	log.Println("Starting application on port", port)
 
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
 	if err != nil {
 		log.Fatal(err)
 	}
