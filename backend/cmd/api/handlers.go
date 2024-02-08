2428e04748af2a4ea9eac334d7fbde0469845fe1
Ikhbar Nurinsani <ikhbarnuri@gmail.com>
2024-02-08 21:04:00 +0700

    backend: ambil data movies dari database

--- backend/cmd/api/handlers.go
+++ backend/cmd/api/handlers.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"backend/internal/models"
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"time"
 )
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
@@ -30,37 +28,11 @@ func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
-	var movies []models.Movie
-
-	releaseDate, _ := time.Parse("2006-01-02", "1986-03-07")
-
-	highLander := models.Movie{
-		ID:          1,
-		Title:       "Highlander",
-		ReleaseDate: releaseDate,
-		RunTime:     116,
-		MPAARating:  "R",
-		Description: "A very nice movie",
-		CreatedAt:   time.Now(),
-		UpdatedAt:   time.Now(),
+	movies, err := app.DB.AllMovies()
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
-
-	movies = append(movies, highLander)
-
-	releaseDate, _ = time.Parse("2006-01-02", "1981-06-12")
-
-	rotl := models.Movie{
-		ID:          2,
-		Title:       "Raiders of the Lost Ark",
-		ReleaseDate: releaseDate,
-		RunTime:     115,
-		MPAARating:  "PG-13",
-		Description: "Another very nice movie",
-		CreatedAt:   time.Now(),
-		UpdatedAt:   time.Now(),
-	}
-
-	movies = append(movies, rotl)
 
 	out, err := json.Marshal(movies)
 	if err != nil {
